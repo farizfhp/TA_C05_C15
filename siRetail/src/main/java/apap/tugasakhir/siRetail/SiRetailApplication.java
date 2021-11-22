089589eb502dac8e61c83b7e3ceb8f8a59095f03
Fahira Anya Katili <fahira.anya@ui.ac.id>
2021-11-22 19:29:20 +0700

    docs: add siretail project

---
+++ siRetail/src/main/java/apap/tugasakhir/siRetail/SiRetailApplication.java
@@ -0,0 +1,13 @@
+package apap.tugasakhir.siRetail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiRetailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiRetailApplication.class, args);
+	}
+
+}
---
+++ siRetail/src/test/java/apap/tugasakhir/siRetail/SiRetailApplicationTests.java
@@ -0,0 +1,13 @@
+package apap.tugasakhir.siRetail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiRetailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
