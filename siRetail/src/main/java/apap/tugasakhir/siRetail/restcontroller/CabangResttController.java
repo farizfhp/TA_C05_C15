61e661d4d552c79a5fa94470b5725e543b9988d7
fahiraanya <90309782+fahiraanya@users.noreply.github.com>
2021-12-07 11:53:41 +0700

    Branch anya (#19)
    * feat: add 'delete cabang' feature
    
    * feat: add 'delete cabang' feature
    
    * feat: fitur 16
    
    * feat: fitur 16 (issue resolved)
    
    * rename CabangResttController

---
+++ siRetail/src/main/java/apap/tugasakhir/siRetail/restcontroller/CabangResttController.java
@@ -0,0 +1,34 @@
+package apap.tugasakhir.siRetail.restcontroller;
+
+import apap.tugasakhir.siRetail.model.CabangModel;
+import apap.tugasakhir.siRetail.service.CabangRestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1")
+public class CabangResttController {
+    @Autowired
+    CabangRestService cabangRestService;
+
+    @PostMapping(value = "/cabang")
+    private CabangModel createCabang(@Valid @RequestBody CabangModel cabang, BindingResult bindingResult) {
+        if(bindingResult.hasFieldErrors()) {
+            throw new ResponseStatusException(
+                    HttpStatus.BAD_REQUEST, "Request body has invalid type or missing field."
+            );
+        } else {
+            return cabangRestService.createCabang(cabang);
+        }
+    }
+    @GetMapping(value = "/list-cabang")
+    private List<CabangModel> retrieveListCabang() {
+        return cabangRestService.retrieveListCabang();
+    }
+}
--- siRetail/src/main/java/apap/tugasakhir/siRetail/service/CabangRestService.java
+++ siRetail/src/main/java/apap/tugasakhir/siRetail/service/CabangRestService.java
@@ -2,6 +2,9 @@ package apap.tugasakhir.siRetail.service;
 
 import apap.tugasakhir.siRetail.model.CabangModel;
 
+import java.util.List;
+
 public interface CabangRestService {
     CabangModel createCabang(CabangModel cabang);
+    List<CabangModel> retrieveListCabang();
 }
