296c929fbb4e9cbdeee8cf5fd9a7644fd8787bb0
Josia Priscilla <josiapriscilla@gmail.com>
2021-12-07 19:36:50 +0700

    feat: fitur 12 unfinished

---
+++ siRetail/src/main/java/apap/tugasakhir/siRetail/rest/Setting.java
@@ -0,0 +1,5 @@
+package apap.tugasakhir.siRetail.rest;
+
+public class Setting {
+    final public static String fitur12Url = "";
+}
--- siRetail/src/main/java/apap/tugasakhir/siRetail/service/CabangRestService.java
+++ siRetail/src/main/java/apap/tugasakhir/siRetail/service/CabangRestService.java
@@ -1,5 +1,7 @@
 package apap.tugasakhir.siRetail.service;
 import apap.tugasakhir.siRetail.model.CabangModel;
+import apap.tugasakhir.siRetail.rest.KuponDetail;
+import reactor.core.publisher.Mono;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -9,4 +11,5 @@ public interface CabangRestService {
     ArrayList<HashMap<String,String>> retrieveListAlamatCabang();
     CabangModel createCabang(CabangModel cabang);
     List<CabangModel> retrieveListCabang();
+    Mono<KuponDetail> listCoupon(Long idItemCabang);
 }
