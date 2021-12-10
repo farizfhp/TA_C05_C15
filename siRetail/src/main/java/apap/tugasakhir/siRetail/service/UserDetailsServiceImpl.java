5b60fe6292fee8db43d89c0ded5cb2dcea2a03d1
farizfhp <70313195+farizfhp@users.noreply.github.com>
2021-12-10 22:36:58 +0700

    Branch fariz (#51)
    * feat: update, view, viewall cabang
    
    * feat: cleaner look
    
    * feat: add item stock webservice
    
    * resolve conflicts
    
    * feat: add item to cabang unfinished
    
    * feat: request to add stok finished
    
    * chore: remove unused imports
    
    * chore: prettier
    
    * feat: deploy app
    
    * chore: add file to gitignore
    
    * chore: add app properties
    
    * feat: add item to cabang finished
    
    * chore: add gitignore lines
    
    * chore: delete app properties
    
    * chore: add userdetailsserviceimpl
    
    * remove file
    
    * resolve

--- siRetail/src/main/java/apap/tugasakhir/siRetail/service/UserDetailsServiceImpl.java
+++ siRetail/src/main/java/apap/tugasakhir/siRetail/service/UserDetailsServiceImpl.java
@@ -14,6 +14,9 @@ import org.springframework.stereotype.Service;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.transaction.Transactional;
+
+@Transactional
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
     @Autowired
