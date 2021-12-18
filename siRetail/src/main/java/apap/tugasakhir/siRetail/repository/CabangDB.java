b35c62523cc4afeef4fe0b09653a9f021b55a701
Fahira Anya Katili <fahira.anya@ui.ac.id>
2021-12-18 18:37:45 +0700

    feat: add Manage Cabang button

--- siRetail/src/main/java/apap/tugasakhir/siRetail/repository/CabangDB.java
+++ siRetail/src/main/java/apap/tugasakhir/siRetail/repository/CabangDB.java
@@ -3,11 +3,7 @@ package apap.tugasakhir.siRetail.repository;
 import apap.tugasakhir.siRetail.model.CabangModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-<<<<<<< HEAD
-public interface CabangDB extends JpaRepository<CabangModel,Long> {
-    <Optional>CabangModel findByIdCabang(Long idCabang);
-=======
+
 public interface CabangDB extends JpaRepository<CabangModel, Long> {
     <Optional> CabangModel findByIdCabang(Long idCabang);
->>>>>>> accbcd8371bb0e66ca9a6820cb41f981176624b7
 }
--- siRetail/src/main/java/apap/tugasakhir/siRetail/security/WebSecurityConfig.java
+++ siRetail/src/main/java/apap/tugasakhir/siRetail/security/WebSecurityConfig.java
@@ -44,10 +44,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
     @Autowired
     private UserDetailsService userDetailsService;
 
-    // @Autowired
-    // public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception{
-    //     auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
-    // }
+     @Autowired
+     public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception{
+         auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
+     }
 
 
     @Autowired
