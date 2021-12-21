32cfbf5ead2c3b1f434f9e70b2d43a923117dfd0
farizfhp <fariz.habibie@ui.ac.id>
2021-12-21 21:49:34 +0700

    feat: finished add item and others

--- siRetail/src/main/java/apap/tugasakhir/siRetail/rest/Setting.java
+++ siRetail/src/main/java/apap/tugasakhir/siRetail/rest/Setting.java
@@ -1,5 +1,5 @@
 package apap.tugasakhir.siRetail.rest;
 
 public class Setting {
-    final public static String fitur12Url = "https://47c07458-4934-42bb-9467-82cc3a4624e2.mock.pstmn.io";
+    final public static String fitur12Url = "https://sibusiness-c05-13.herokuapp.com/api/v1/";
 }
